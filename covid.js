b1cff51935838766629dd55fb8d33998ef83f325
Józsi <git@jozsi.ro>
2020-03-23 23:02:30 +0200

    Update graph styling

--- covid.js
+++ covid.js
@@ -183,15 +183,8 @@ function chartDataset(name, data, color) {
     label: name,
     data: data,
     backgroundColor: Array(data.length).fill(color),
-    borderColor: [
-      'rgba(255, 99, 132, 1)',
-      'rgba(54, 162, 235, 1)',
-      'rgba(255, 206, 86, 1)',
-      'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)',
-    ],
-    borderWidth: 1,
+    barPercentage: 1.0,
+    categoryPercentage: 1.0,
   };
 }
 
